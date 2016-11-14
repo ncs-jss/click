f856e4a33f34fedf9dce8131be07176b32a69877
sratmele <siddhartharatmele@gmail.com>
2016-11-15 01:42:07 +0530

    Background tasks login and storing token in SharedPrefernces

--- app/src/main/java/com/hackncs/click/Splash.java
+++ app/src/main/java/com/hackncs/click/Splash.java
@@ -1,31 +1,36 @@
 package com.hackncs.click;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -44,10 +49,13 @@ public class Splash extends Activity {
     final int INITIAL_DISPLAY = 0, LOGIN_DISPLAY = 1;
     EditText username, password;
     Button submit;
-
-    boolean isConnected;
-
+    TextView conn_err_msg;
+    boolean isConnected, logged_in;
     View.OnClickListener submitListener;
+    ProgressDialog progressDialog;
+    String sUsername, sPassword;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,7 +66,24 @@ public class Splash extends Activity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                changeView();
+                if (isConnected) {
+                    if (logged_in) {
+                        Intent intent = new Intent(Splash.this, MainActivity.class);
+                        intent.putExtra("mode", "ONLINE_MODE");
+                        startActivity(intent);
+                    } else {
+                        changeView();
+                    }
+                } else {
+
+                    if (logged_in) {
+                        Intent intent = new Intent(Splash.this, MainActivity.class);
+                        intent.putExtra("mode", "OFFLINE_MODE");
+                        startActivity(intent);
+                    } else {
+                        conn_err_msg.setVisibility(View.VISIBLE);
+                    }
+                }
             }
         }, 3750);
     }
@@ -68,12 +93,15 @@ public class Splash extends Activity {
             case INITIAL_DISPLAY:
                 logo = (ImageView) findViewById(R.id.ivLogo);
                 loader = (ProgressBar) findViewById(R.id.pbLoader);
+                conn_err_msg = (TextView) findViewById(R.id.tvConnErrMsg);
                 progress = 0;
+                sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
                 break;
             case LOGIN_DISPLAY:
                 username = (EditText)findViewById(R.id.etUsername);
                 password = (EditText)findViewById(R.id.etPassword);
                 submit = (Button)findViewById(R.id.bSubmit);
+                submit.setClickable(false);
                 break;
         }
     }
@@ -81,6 +109,7 @@ public class Splash extends Activity {
     private void backgroundTasks() {
         int x = 0;
         isConnected = isOnline();
+        logged_in = sharedPreferences.getBoolean("com.hackncs.click.LOGGED_IN", false);
         connectFolder();
         while (progress <= 100) {
             if (progress % 5 == 0)
@@ -119,36 +148,10 @@ public class Splash extends Activity {
         submitListener =  new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final String sUsername = username.getText().toString().trim();
-                final String sPassword = password.getText().toString().trim();
-                StringRequest stringRequest = new StringRequest(Request.Method.POST, LOGIN_URL,
-                        new Response.Listener<String>() {
-                            @Override
-                            public void onResponse(String response) {
-                                try {
-                                    JSONObject jsonObject = new JSONObject(response);
-                                    Toast.makeText(Splash.this, jsonObject.getString("token"), Toast.LENGTH_SHORT).show();
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        },
-                        new Response.ErrorListener() {
-                            @Override
-                            public void onErrorResponse(VolleyError error) {
-                                Toast.makeText(Splash.this, error.getMessage(), Toast.LENGTH_LONG).show();
-                            }
-                        }){
-                    @Override
-                    protected Map<String, String> getParams() throws AuthFailureError {
-                        Map<String, String> map = new HashMap<>();
-                        map.put("username",sUsername);
-                        map.put("password",sPassword);
-                        return map;
-                    }
-                };
-                RequestQueue requestQueue = Volley.newRequestQueue(Splash.this);
-                requestQueue.add(stringRequest);
+                progressDialog = new ProgressDialog(Splash.this);
+                sUsername = username.getText().toString().trim();
+                sPassword = password.getText().toString().trim();
+                new LoginTasks().execute();
             }
         };
         submit.setOnClickListener(submitListener);
@@ -162,8 +165,36 @@ public class Splash extends Activity {
 
     public boolean isOnline() {
         ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo netInfo = connectivityManager.getActiveNetworkInfo();
-        if (netInfo != null && netInfo.isConnected()) {
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        /*NetworkInfo netInfo_bluetooth = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_BLUETOOTH);
+        //NetworkInfo netInfo_dummy = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_DUMMY);
+        //NetworkInfo netInfo_ethernet = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_ETHERNET);
+        NetworkInfo netInfo_mobile = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        NetworkInfo netInfo_mobile_dun = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE_DUN);
+        //NetworkInfo netInfo_vpn = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_VPN);
+        NetworkInfo netInfo_wifi = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        //NetworkInfo netInfo_wimax = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIMAX);
+        if (
+                (       netInfo_bluetooth != null ||
+                        //netInfo_dummy != null ||
+                        //netInfo_ethernet != null ||
+                        netInfo_mobile != null ||
+                        netInfo_mobile_dun != null ||
+                        //netInfo_vpn != null ||
+                        netInfo_wifi != null)
+                        //netInfo_wimax != null)
+
+                        &&
+
+                (       netInfo_bluetooth.isConnected() ||
+                        //netInfo_dummy.isConnected() ||
+                        //netInfo_ethernet.isConnected() ||
+                        netInfo_mobile.isConnected() ||
+                        netInfo_mobile_dun.isConnected() ||
+                        //netInfo_vpn.isConnected() ||
+                        netInfo_wifi.isConnected())
+                        //netInfo_wimax.isConnected())
+                ) {
             runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
@@ -171,7 +202,17 @@ public class Splash extends Activity {
                 }
             });
             return true;
-        } else {
+        }*/
+        if (networkInfo != null && networkInfo.isConnected()) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(getApplicationContext(), "Connected!", Toast.LENGTH_SHORT).show();
+                }
+            });
+            return true;
+        }
+        else {
             runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
@@ -209,6 +250,79 @@ public class Splash extends Activity {
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
             animate(INITIAL_DISPLAY);
+        }
+    }
+
+    public class LoginTasks extends AsyncTask<String, Integer, String> {
+
+        String token = "", group = "", user_id = "";
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog.setMessage("Logging you in...");
+            progressDialog.show();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            progressDialog.dismiss();
+                if (!s.equals("ok")) {
+                    Toast.makeText(Splash.this, "Login failed!", Toast.LENGTH_SHORT).show();
+                } else {
+                    editor = sharedPreferences.edit();
+                    editor.putBoolean("com.hackncs.click.LOGGED_IN", true);
+                    editor.putString("com.hackncs.click.TOKEN", token);
+                    editor.putString("com.hackncs.click.GROUP", group);
+                    editor.putString("com.hackncs.click.USER_ID", user_id);
+                    editor.commit();
+                    Intent intent = new Intent(Splash.this, MainActivity.class);
+                    intent.putExtra("mode", "ONLINE_MODE");
+                    startActivity(intent);
+                }
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            final String[] status = {"fail"};
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, Endpoints.LOGIN_URL,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            try {
+                                JSONObject jsonObject = new JSONObject(response);
+                                token = jsonObject.getString("token");
+                                group = jsonObject.getString("group");
+                                user_id = jsonObject.getString("user_id");
+                                //Toast.makeText(Splash.this, token+"\n"+group+"\n"+user_id, Toast.LENGTH_SHORT).show();
+                                status[0] = "ok";
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(Splash.this, error.toString(), Toast.LENGTH_LONG).show();
+                        }
+                    }){
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> map = new HashMap<>();
+                    map.put("username",sUsername);
+                    map.put("password",sPassword);
+                    return map;
+                }
+            };
+            RequestQueue requestQueue = Volley.newRequestQueue(Splash.this);
+            requestQueue.add(stringRequest);
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return status[0];
         }
     }
 }
